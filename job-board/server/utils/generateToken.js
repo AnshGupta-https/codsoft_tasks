afbd5aaa6b010a6dc4dfd1fa43acb353d49b0518
Ansh Gupta <anshgupta51735@gmail.com>
2026-07-13 20:15:42 +0530

    Delete job-board directory

--- job-board/server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-function generateToken(userId) {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  })
-}
-
-export default generateToken
